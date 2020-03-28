de7a0ab0765e3a9e984345bffff51e668bb606a3
Marlon Torres <marlont@bluetrailsoft.com>
2020-03-28 13:08:29 -0600

    new features added

--- file.js
+++ file.js
@@ -2,3 +2,7 @@ console.log('begin');
 console.log('fetures added');
 console.log('all good');
 console.log('beta version done');
+console.log('new features');
+console.log('ajalas esta bien perro');
+console.log('soy la mera pistola');
+console.log('ya acabe');
